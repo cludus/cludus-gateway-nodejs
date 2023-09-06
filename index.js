127d88004e5a9ca06beea257b0fad7de523d1533
Michel Atucha <miche.atucha@gmail.com>
2023-09-06 15:06:35 -0500

    env and nodemon

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
 import { WebSocketServer } from 'ws';
+import 'dotenv/config';
+
+const port = process.env.WS_PORT || 8080;
+const path = process.env.WS_PATH || '/websocket';
 
 const server = new WebSocketServer({
-  port: 8080,
-  path: '/websocket',
+  port: port,
+  path: path,
+}, () => {
+  console.log('Cludus Gateway webSocket server started on port %i with path %s', port, path);
 });
 
 server.on('connection', (socket, request) => {
   const clientToken = request.headers['authentication'];
-  if (!clientToken) {
-    socket.terminate();
-  } else {
+  if (!!clientToken) {
     setupClient(clientToken, socket);
+  } else {
+    socket.terminate();
   }
 });
 
 const clients = {};
 const setupClient = (clientToken, socket) => {
   clients[clientToken] = socket;
-  console.log('Client %s connected!', clientToken);
+  console.debug('Client %s connected!', clientToken);
 
   socket.send('Welcome to Cludus');
 
   socket.on('message', (data) => {
-    console.log('Client %s message received: %s', clientToken, data);
+    console.debug('Client %s message received: %s', clientToken, data);
+    socket.send(`Echo: ${data}`);
   });
 
   socket.on('close', () => {
-    console.log('Client %s disconnected!', clientToken);
+    console.debug('Client %s disconnected!', clientToken);
     clients[clientToken] = null;
   });
 };
